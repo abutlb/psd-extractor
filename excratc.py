7196417fdf66aec38af529ba2dcdb20ac0f21616
abdullah <abutlb10015@gmail.com>
2023-01-18 13:40:39 +0300

    Add files via upload

---
+++ excratc.py
@@ -0,0 +1,24 @@
+from psd_tools import PSDImage
+import os
+
+
+files = os.listdir("in\\")
+i = 1
+
+for file in files:
+        psd = PSDImage.open(f'in\\{file}')
+        print(f"working in file {i} of {len(files)}")
+        ln = 1
+        for layer in psd:
+            if layer.is_group():
+                print(f"working on layer {ln} of {len(psd)}")
+                for l in layer:
+                    layer_image = l.composite()
+                    layer_image.save(f"out\\file{i}layer{ln}.png")
+                    ln = ln + 1
+            else:
+                print(f"working on layer {ln} of {len(psd)}")
+                layer_image = layer.composite()
+                layer_image.save(f"out\\file{i}layer{ln}.png")
+                ln = ln + 1
+        i = i + 1
